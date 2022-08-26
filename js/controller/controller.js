12d1cad1b24951f92188b590ac5f4aae9560d773
bakhtiyorjondadajonov <bakhtiyorjondadajonov@gmail.com>
2022-08-26 12:54:32 +0900

    perfumes section done

--- js/controller/controller.js
+++ js/controller/controller.js
@@ -1,2 +1,38 @@
 import { a } from "../view/view.js";
 console.log("a: ", a);
+const seasonalCarousel = document.querySelector(".seasonal__carousel");
+import Swiper from "https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.esm.browser.min.js";
+
+const swiper = new Swiper(".mySwiper", {
+  slidesPerView: 2,
+  grid: {
+    rows: 2,
+  },
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    0: {
+      grid: {
+        rows: 1,
+      },
+    },
+    800: {
+      grid: {
+        rows: 2,
+      },
+    },
+  },
+});
+const swiperParfumes = new Swiper(".swiper__parfumes", {
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
--- js/view/view.js
+++ js/view/view.js
@@ -27,3 +27,74 @@ export const carouselFn = (elem, btnLeft, btnRight, pageDots) => {
 };
 
 carouselFn(arrivalsCarousel, [false, false], false);
+
+//---------- RENDER PRODUCTS CARD
+const seasonalWrapper = document.querySelector(".swiper-wrapper");
+
+const seasonalData = [
+  {
+    img: "product-1.png",
+    name: "Gucci",
+    description: "Zipper mini dress",
+    price: 100,
+  },
+  {
+    img: "product-2.png",
+    name: "Gucci",
+    description: "Zipper mini dress",
+    price: 100,
+  },
+  {
+    img: "product-3.png",
+    name: "Gucci",
+    description: "Zipper mini dress",
+    price: 100,
+  },
+  {
+    img: "product-1.png",
+    name: "Anton",
+    description: "Mix Pattern Leather Jascet",
+    price: 100,
+  },
+  {
+    img: "product-1.png",
+    name: "Gucci",
+    description: "Zipper mini dress",
+    price: 100,
+  },
+  {
+    img: "product-2.png",
+    name: "Gucci",
+    description: "Zipper mini dress",
+    price: 100,
+  },
+  {
+    img: "product-3.png",
+    name: "Gucci",
+    description: "Zipper mini dress",
+    price: 100,
+  },
+  {
+    img: "product-1.png",
+    name: "Anton",
+    description: "Mix Pattern Leather Jascet",
+    price: 100,
+  },
+];
+seasonalData.forEach((data) => {
+  const html = `
+  <div class="swiper-slide swiper-slide--seasonal">
+  <div class="product-card product-card--seasonal">
+    <div class="product__img product__img--seasonal">
+      <div
+        class="like-btn like-btn--seasonal like-filled like-filled"
+      ></div>
+      <img src="./public/${data.img}" alt="" />
+    </div>
+    <div class="product__name">${data.name}</div>
+    <div class="product__description">${data.description}</div>
+    <div class="product__price">$${data.price}.00</div>
+  </div>
+</div>`;
+  seasonalWrapper.insertAdjacentHTML("beforeend", html);
+});
